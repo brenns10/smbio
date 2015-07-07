Fix entropy calculation bug.

smbio/math/information.py
@@ -24,7 +24,7 @@ def entropy(l):
     probabilities = np.bincount(l) / len(l)
     with np.errstate(divide='ignore'):  # ignore log(0) errors, we'll handle
         log_probabilities = np.log2(probabilities)
-        log_probabilities[np.isnan(log_probabilities)] = 0
+        log_probabilities[~np.isfinite(log_probabilities)] = 0
     return -np.sum(probabilities * log_probabilities)
 
 